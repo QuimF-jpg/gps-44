0f5262d85227fa7d8ba724a1f8f0b56ee4efa38f
Aram Lopez <aram.lopez@estudiantat.upc.edu>
2024-12-19 14:47:45 +0000

    Update test.py dudoso

--- test.py
+++ test.py
@@ -4,15 +4,15 @@ import transform
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_is_upper(self):
+    def test_to_upper(self):
         sting = transform.to_upper_case("hello")
         self.assertEqual(sting, "HELLO")
 
-    def test_is_lower(self):
+    def test_to_lower(self):
         sting = transform.to_lower_case("HELLO")
         self.assertEqual(sting, "hello")
 
-    def test_is_capitalize(self):
+    def test_to_capitalize(self):
         sting = transform.to_capitalize("HELLO")
         self.assertEqual(sting, "Hello")
 
